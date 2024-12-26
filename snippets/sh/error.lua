977438ac1854abb6a042dc0995ae6265ebd24786
am2b <20837051+am2b@users.noreply.github.com>
2024-12-27 01:35:35 +0800

    add snippet:error.lua

---
+++ snippets/sh/error.lua
@@ -0,0 +1,44 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+return {
+    s(
+        {
+            trig = ";ferr",
+            dscr = "error message function",
+            snippetType = "autosnippet",
+        },
+
+        fmta(
+            [[
+                error_msg() {
+                    printf "ERROR:\nSCRIPT:%s\nFUNC:%s\nLINE:%d\n" "$(basename "${0}")" "${FUNCNAME[1]}" "$1"
+                }
+
+                <>
+            ]],
+
+            { i(0) }
+        )
+    ),
+
+    s(
+        {
+            trig = ";perr",
+            dscr = "print error message",
+            snippetType = "autosnippet",
+        },
+
+        fmta(
+            [[
+                error_msg "$LINENO"
+
+                <>
+            ]],
+
+            { i(0) }
+        )
+    ),
+}
