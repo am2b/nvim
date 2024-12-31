9de51be1a08bcced47829be078803d5baa26e6cd
am2b <20837051+am2b@users.noreply.github.com>
2025-01-01 01:59:44 +0800

    fix core/keymaps-quickfix-location-list.lua:make n/N to support location list

--- lua/core/keymaps-quickfix-location-list.lua
+++ lua/core/keymaps-quickfix-location-list.lua
@@ -8,9 +8,21 @@ keymap.set("n", "<space>cc", "<cmd>cclose<cr>", { desc = "Normal:Close quickfix 
 keymap.set("n", "<space>lo", "<cmd>lopen<cr>", { desc = "Normal:Open location list" })
 keymap.set("n", "<space>lc", "<cmd>lclose<cr>", { desc = "Normal:Close location list" })
 
---当打开quickfix窗口时,n和N用来模拟:cnext和:cprev
---判断quickfix是否打开且有内容的函数
-local function is_quickfix_open()
+--n和N用来模拟:cnext和:cprev或者:lnext和lprev
+--优先对Location List执行操作,否则操作Quickfix List
+
+--检测当前窗口是否有Location List
+local function is_locationlist_open()
+    for _, win in ipairs(vim.fn.getwininfo()) do
+        if win.loclist == 1 then
+            return true
+        end
+    end
+    return false
+end
+
+--检查是否有Quickfix List
+local function is_quickfixlist_open()
     for _, win in ipairs(vim.api.nvim_list_wins()) do
         if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win), "filetype") == "qf" then
             return true
@@ -22,17 +34,25 @@ end
 --动态调整n和N的行为
 vim.keymap.set("n", "n", function()
     --仅当按下n或N时才会触发窗口遍历
-    if is_quickfix_open() then
-        return ":cnext<cr>"
-    else
-        return "n"
+    if is_locationlist_open() then
+        return ":lnext<cr>"
     end
+
+    if is_quickfixlist_open() then
+        return ":cnext<cr>"
+    end
+
+    return "n"
 end, { expr = true, noremap = true, silent = true })
 
 vim.keymap.set("n", "N", function()
-    if is_quickfix_open() then
-        return ":cprev<cr>"
-    else
-        return "N"
+    if is_locationlist_open() then
+        return ":lprev<cr>"
     end
+
+    if is_quickfixlist_open() then
+        return ":cprev<cr>"
+    end
+
+    return "N"
 end, { expr = true, noremap = true, silent = true })
