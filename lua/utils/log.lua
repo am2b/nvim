2719089238be91a1d6a3922516d6b66b7b944e59
am2b <20837051+am2b@users.noreply.github.com>
2025-03-08 13:43:11 +0800

    update utils/log.lua:dynamic type

--- lua/utils/log.lua
+++ lua/utils/log.lua
@@ -2,23 +2,18 @@ local M = {}
 
 local log_file_path = vim.fn.expand("/tmp/nvim_debug.log")
 
-M.log_string = function(msg_string)
+M.log = function(msg)
     local file = io.open(log_file_path, "a")
     if file then
         local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local log_message = string.format("[%s] %s\n", timestamp, msg_string)
-        file:write(log_message)
-        file:close()
-    else
-        vim.notify("Failed to open log file: " .. log_file_path, vim.log.levels.ERROR)
-    end
-end
 
-M.log_table = function(msg_table)
-    local file = io.open(log_file_path, "a")
-    if file then
-        local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local log_message = string.format("[%s] %s\n", timestamp, vim.inspect(msg_table))
+        local log_message = ""
+        if type(msg) == "string" then
+            log_message = string.format("[%s] %s\n", timestamp, msg)
+        elseif type(msg) == "table" then
+            log_message = string.format("[%s] %s\n", timestamp, vim.inspect(msg))
+        end
+
         file:write(log_message)
         file:close()
     else
