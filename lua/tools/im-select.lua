ce588ca3b7e3349dcb1f175279f91116df1ae8ca
am2b <20837051+am2b@users.noreply.github.com>
2025-07-07 10:57:47 +0800

    switch to English input method when leaving command mode

--- lua/tools/im-select.lua
+++ lua/tools/im-select.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("InsertLeave", {
         os.execute("im-select com.apple.keylayout.ABC")
     end,
 })
+
+--离开命令模式(:/?)时切换到英文输入法
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+    callback = function()
+        os.execute("im-select com.apple.keylayout.ABC")
+    end,
+})
