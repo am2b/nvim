f9e7c500bd5e46f1193189b6450e1eb727aebd52
am2b <20837051+am2b@users.noreply.github.com>
2025-07-27 13:06:50 +0800

    disable keymaps of location list

--- lua/core/keymaps-quickfix-location-list.lua
+++ lua/core/keymaps-quickfix-location-list.lua
@@ -5,5 +5,5 @@ keymap.set("n", "<space>co", "<cmd>copen<cr>", { desc = "Normal:Open quickfix li
 keymap.set("n", "<space>cc", "<cmd>cclose<cr>", { desc = "Normal:Close quickfix list" })
 
 --location list
-keymap.set("n", "<space>lo", "<cmd>lopen<cr>", { desc = "Normal:Open location list" })
-keymap.set("n", "<space>lc", "<cmd>lclose<cr>", { desc = "Normal:Close location list" })
+--keymap.set("n", "<space>lo", "<cmd>lopen<cr>", { desc = "Normal:Open location list" })
+--keymap.set("n", "<space>lc", "<cmd>lclose<cr>", { desc = "Normal:Close location list" })
