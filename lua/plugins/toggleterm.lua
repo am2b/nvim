487c853c19e696a2116342f8c6d660f729842b24
am2b <20837051+am2b@users.noreply.github.com>
2026-08-26 21:52:32 +0800

    优化:plugins/toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ return {
 
     opts = {
         start_in_insert = true,
-        --保持终端打开状态,这样终端关闭后就不会立即销毁。
+        --在多个终端窗口之间切换时,记住每个终端所处的状态(insert/normal模式)
         persist_mode = true,
         --设定水平和垂直终端的默认大小
         --size = 20,
@@ -28,9 +28,12 @@ return {
 
         vim.keymap.set("t", "<c-k>", "<cmd>wincmd k<cr>", { desc = "Toggleterm:Move cursor between windows" })
         vim.keymap.set("t", "<c-j>", "<cmd>wincmd j<cr>", { desc = "Toggleterm:Move cursor between windows" })
+        --在有的终端里,<c-h>和退格键<bs>是同一个控制字符(0x08),所以可能会导致退格失效(删不掉字符)
         vim.keymap.set("t", "<c-h>", "<cmd>wincmd h<cr>", { desc = "Toggleterm:Move cursor between windows" })
         vim.keymap.set("t", "<c-l>", "<cmd>wincmd l<cr>", { desc = "Toggleterm:Move cursor between windows" })
 
         vim.keymap.set("t", "<esc>", "<cmd>ToggleTerm<cr>", { desc = "Toggleterm:Toggle terminal" })
     end
 }
+
+--toggleterm的终端默认就是常驻的(:ToggleTerm关掉只是隐藏,不会销毁,除非真的退出shell)
