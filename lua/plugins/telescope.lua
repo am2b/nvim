434160fbefa782128ea188ee93ac91681337bcef
am2b <20837051+am2b@users.noreply.github.com>
2026-09-24 08:59:24 +0800

    整理telescope的快捷键和命令

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,15 @@
 --https://github.com/nvim-telescope/telescope.nvim
 --https://github.com/nvim-telescope/telescope-fzf-native.nvim
 
+--依赖的软件:ripgrep,fd
+
+--?:show mappings for picker actions (normal mode)
+--<c-/>:show mappings for picker actions (insert mode)
+--<c-u>:scroll up in preview window
+--<c-d>:scroll down in preview window
+--<c-q>:send all items to quickfix
+--<M-q>:send all selected items to quickfix
+
 return {
     'nvim-telescope/telescope.nvim',
 
@@ -102,86 +111,9 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.lsp_document_symbols, { desc = 'Telescope:find document symbols' })
         --search symbols(整个项目:类名,函数名,变量等)
         --dynamic:自动用光标下的词作为初始搜索词(不用手动输入),并随打字实时刷新
-        vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols, { desc = "Telescope:find workspace symbols" })
+        vim.keymap.set('n', '<leader>fs', builtin.lsp_dynamic_workspace_symbols,
+            { desc = "Telescope:find workspace symbols" })
 
-        --vim pickers
-        --list buffers
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope:list buffers" })
-
-        --list marks
-        vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = "Telescope:list marks" })
-
-        --list registers,pastes the contents of the register on <cr>
-        vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = "Telescope:list registers" })
-
-        --list keymappings of normal mode
-        vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Telescope:list keymappings of normal mode" })
-
-        --lists vim options,allows you to edit the current value on <cr>
-        vim.keymap.set('n', '<leader>fv', builtin.vim_options, { desc = "Telescope:list vim options" })
-
-        --lists manpage entries
-        vim.keymap.set('n', '<leader>fa', builtin.man_pages, { desc = "Telescope:list manpage entries" })
-
-        --list jumplist
-        vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "Telescope:list jumplist" })
-
-        --lists items in the quickfix list
-        --vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = "Telescope:list items in the quickfix list" })
-
-        --lists items from the current window's location list
-        --vim.keymap.set('n', '<leader>fl', builtin.loclist, { desc = "Telescope:list items from the window's location list" })
-
-        --lists available plugin/user commands and runs them on <cr>
-        vim.keymap.set('n', '<leader>fx', builtin.commands, { desc = "Telescope:list plugin/user commands" })
-
-        --lists vim autocommands and goes to their declaration on <cr>
-        vim.keymap.set('n', '<leader>fy', builtin.autocommands, { desc = "Telescope:list autocommands" })
-
-        --lists spelling suggestions for the current word under the cursor, replaces word with selected suggestion on <cr>
-        vim.keymap.set('n', '<leader>fw', builtin.spell_suggest, { desc = "Telescope:spelling suggestions" })
-
-        --lists all available filetypes and apply to current file on <cr>
-        vim.keymap.set('n', '<leader>ft', builtin.filetypes, { desc = "Telescope:list available filetypes" })
-
-        --lists built-in pickers and run them on <cr>
-        vim.keymap.set('n', '<leader>fd', builtin.builtin,
-            { desc = "Telescope:list built-in pickers and run them on <cr>" })
-
-        --列出lsp所报的问题:错误,警告,提示等
-        vim.keymap.set('n', '<leader>fg', builtin.diagnostics, { desc = "Telescope:list diagnostics" })
-
-        --require('utils.notify').notify_with_timeout("插件:telescope 已加载", 3000)
+        require("plugins.telescope-cmds")(builtin)
     end
 }
-
---telescope.nvim是neovim社区广泛使用的模糊搜索插件,基于lua编写,提供了强大的查找和搜索功能。其主要功能包括：
---1,文件查找:快速查找文件,尊重.gitignore,还可以自定义搜索工具如rg(ripgrep)。
---2,字符串搜索:在当前工作目录中搜索字符串,并根据输入实时更新结果。
---3,缓冲区和书签列表:列出所有缓冲区,书签和其他与neovim状态相关的内容,如注册表,跳转列表等。
---4,扩展支持:支持加载扩展,如fzf扩展,增强模糊搜索能力。
-
---suggested dependencies
---BurntSushi/ripgrep is required for live_grep and grep_string and is the first priority for find_files
---we also suggest you install one native telescope sorter to significantly improve sorting performance:telescope-fzf-native.nvim
-
---optional dependencies
---sharkdp/fd (finder)
-
---:help telescope
---:help telescope.setup(),see default configuration options
---:help telescope.builtin,see picker specific opts
---:help telescope.command
---:help telescope.layout
---:help telescope.resolve
---:lua require('telescope.builtin').find_files({layout_strategy='vertical',layout_config={width=0.5}})
---:checkhealth telescope
-
---?:show mappings for picker actions (normal mode)
---<c-/>:show mappings for picker actions (insert mode)
---<c-u>	scroll up in preview window
---<c-d>	scroll down in preview window
---<c-q> send all items to quickfix
---<M-q>	send all selected items to quickfix
-
---available keys:c,g,n,z
